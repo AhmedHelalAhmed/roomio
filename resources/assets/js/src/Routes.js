b41bf541ebae914545cb350274e1a379df254121
alexcushing <alexander_cushing@student.uml.edu>
2017-03-08 13:26:34 -0500

    built login, unstyled

--- resources/assets/js/src/Routes.js
+++ resources/assets/js/src/Routes.js
@@ -3,12 +3,14 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import AppContainer from './containers/AppContainer';
 import RoomContainer from './containers/RoomContainer';
 import TopicContainer from './containers/TopicContainer';
+import Login from './components/Login';
 
 // router would go here.
 const Routes = () => {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={AppContainer}>
+        <Route path="/signin" component={Login} />
         <Route path="/room/:roomName" component={RoomContainer} />
         <Route path="/room/:roomName/topic/:topicRef" component={TopicContainer} />
       </Route>
