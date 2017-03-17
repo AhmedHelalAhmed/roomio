6c77a74779e7f72eb3b1fdfaf5b2499842e57089
Tyler Bainbridge <contact@tylerbainbridge.com>
2017-03-17 16:06:17 -0400

    auth is fixed

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,18 @@
  * /login and /register routes.
  */
 // Auth::routes();
+Route::group([ 'prefix' => 'api' ], function () {
+  Route::post('login', ['as' => 'login.post', 'uses' => 'Auth\LoginController@login']);
+  Route::post('register', ['as' => 'register.post', 'uses' => 'Auth\RegisterController@register']);
+});
+
 
 Route::get('/logout', function () {
     Auth::logout();
     return redirect()->intended('/');
 });
 
-// for all routes that aren't auth, send the index.html (routing handled in react).
+// for all routes that aren't auth, send the index view (routing handled in react).
 Route::get('/{all}', function ($all) {
-    // dd(Auth::user());
     return view('index');
 })->where(['all' => '.*']);
