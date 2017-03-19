a4c54ccb70328a7392e9d7fc313913ce8aa1a376
alex cushing <alexander_cushing@student.uml.edu>
2017-03-19 13:12:15 -0400

    styled all links and updated readme

--- resources/assets/js/src/components/Profile.js
+++ resources/assets/js/src/components/Profile.js
@@ -7,9 +7,14 @@ const Profile = ({username, profile}) => {
     <div className="sharedContainer">
       <div className="sharedTitleSep usernameOuter">
         <h1 className="username">{username}</h1>
+        <span className="profileDate">created on {profile.created_at}</span>
       </div>
       <div className="profileScore spaceAround">
-        <span>Rooms - {profile.rooms.length}</span>
+        {
+          profile.rooms.length != 1 ? 
+          <span>Admin of {profile.rooms.length} Rooms</span>
+           : <span>Admin of {profile.rooms.length} Room</span>
+        }
         <span>Topics - {profile.topics.length}</span>
       </div>
       <hr className="profileSeperator" />
