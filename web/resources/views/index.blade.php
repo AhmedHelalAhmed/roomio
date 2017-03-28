26aafec0e32164617e7e6eebe7468205a362fd36
Tyler Bainbridge <contact@tylerbainbridge.com>
2017-03-27 22:57:07 -0400

    index

---
+++ web/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+	      <link rel="shortcut icon" href="http://i.imgur.com/X9LSYcX.png">
+        <title>Roomio</title>
+        <script src="https://use.fontawesome.com/b4eac05af7.js"></script>
+        <script type="text/javascript">
+            @if(Auth::check())
+                window.user = {
+                    token: "{{ Auth::user()->api_token }}",
+                    username: "{{ Auth::user()->username }}",
+                    email: "{{ Auth::user()->email }}"
+                };
+             @else
+                window.user = null;
+            @endif
+        </script>
+    </head>
+    <body>
+        <div id="root"></div>
+        @if(env('APP_ENV') === 'dev')
+            <script src="http://localhost:8080/public/bundle.js" charset="utf-8"></script>
+            <!--<script src="js/bundle.js" charset="utf-8"></script>-->
+        @else
+            <script src="http://roomio-web.us-west-2.elasticbeanstalk.com/js/bundle.js" charset="utf-8"></script>
+        @endif
+    </body>
+</html>
