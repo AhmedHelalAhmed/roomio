1f7021cdfa568e59c3eb70c56be1104efb7918bb
alex cushing <alexander_cushing@student.uml.edu>
2017-04-19 19:42:54 -0400

    prettier-ed the modal file

--- web/resources/assets/js/src/components/Modal.js
+++ web/resources/assets/js/src/components/Modal.js
@@ -45,68 +45,89 @@ const loginStyle = {
 class ModalContainer extends Component {
   state = {
     closing: false,
-  }
-  
+  };
+
   closeModalFirst = () => {
-    this.setState({closing: true});
-    setTimeout(() => {
-      this.setState({closing: false});
-      this.props.closeModal()
-    }, 300);
-  }
+    this.setState({ closing: true });
+    setTimeout(
+      () => {
+        this.setState({ closing: false });
+        this.props.closeModal();
+      },
+      300,
+    );
+  };
 
   render() {
     return this.props.type === 'topic'
-      ? <div className={this.state.closing ? 'customBackdropFaded' : 'customBackdrop'}><Modal
-        isOpen={this.props.modalIsOpen}
-        onRequestClose={this.closeModalFirst}
-        style={this.state.closing ? customStylesFadeOut : customStyles}
-        contentLabel="Topic"
-      >
-        <FontAwesome
-          name="times"
-          className="closeModal"
-          onClick={this.closeModalFirst}
-        />
-        <h2 ref="subtitle">{this.props.topic.title}</h2>
-        <h4 ref="subtitle">Description</h4>
-        <p>{this.props.topic.description}</p>
-      </Modal></div>
-      : this.props.type === 'room'
-          ? <div className={this.state.closing ? 'customBackdropFaded' : 'customBackdrop'}><Modal
+      ? <div
+          className={
+            this.state.closing ? 'customBackdropFaded' : 'customBackdrop'
+          }
+        >
+          <Modal
             isOpen={this.props.modalIsOpen}
             onRequestClose={this.closeModalFirst}
             style={this.state.closing ? customStylesFadeOut : customStyles}
-            contentLabel="Room"
+            contentLabel="Topic"
           >
             <FontAwesome
               name="times"
               className="closeModal"
               onClick={this.closeModalFirst}
             />
-            <h2 className="adminTag" ref="subtitle">
-              <FontAwesome name="user" className="usSep" />
-              <Link to={`/user/${this.props.admin}`}>
-                {this.props.admin}
-              </Link>
-              <span className='modalAdmin'>Room Admin</span>
-            </h2>
+            <h2 ref="subtitle">{this.props.topic.title}</h2>
             <h4 ref="subtitle">Description</h4>
-            <p>{this.props.description}</p>
-          </Modal></div>
-          : <div className={this.state.closing ? 'customBackdropFaded' : 'customBackdrop'}><Modal
-            isOpen={this.props.modalIsOpen}
-            onRequestClose={this.props.closeModal}
-            style={loginStyle}
-            contentLabel="create"
-          >
-            <FontAwesome
-              className="topicEx"
-              name="times"
-              onClick={this.props.closeModal}
-            />
-            <CreateTopic room={this.props.room} />
-          </Modal></div>;
+            <p>{this.props.topic.description}</p>
+          </Modal>
+        </div>
+      : this.props.type === 'room'
+          ? <div
+              className={
+                this.state.closing ? 'customBackdropFaded' : 'customBackdrop'
+              }
+            >
+              <Modal
+                isOpen={this.props.modalIsOpen}
+                onRequestClose={this.closeModalFirst}
+                style={this.state.closing ? customStylesFadeOut : customStyles}
+                contentLabel="Room"
+              >
+                <FontAwesome
+                  name="times"
+                  className="closeModal"
+                  onClick={this.closeModalFirst}
+                />
+                <h2 className="adminTag" ref="subtitle">
+                  <FontAwesome name="user" className="usSep" />
+                  <Link to={`/user/${this.props.admin}`}>
+                    {this.props.admin}
+                  </Link>
+                  <span className="modalAdmin">Room Admin</span>
+                </h2>
+                <h4 ref="subtitle">Description</h4>
+                <p>{this.props.description}</p>
+              </Modal>
+            </div>
+          : <div
+              className={
+                this.state.closing ? 'customBackdropFaded' : 'customBackdrop'
+              }
+            >
+              <Modal
+                isOpen={this.props.modalIsOpen}
+                onRequestClose={this.props.closeModal}
+                style={loginStyle}
+                contentLabel="create"
+              >
+                <FontAwesome
+                  className="topicEx"
+                  name="times"
+                  onClick={this.props.closeModal}
+                />
+                <CreateTopic room={this.props.room} />
+              </Modal>
+            </div>;
   }
 }
 
